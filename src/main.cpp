c4ff5e0152ca381a39fd8b314f23defdd468fd06
Kirakos <henacodes@gmail.com>
2026-07-24 05:50:49 +0300

    initialized project

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include "raylib.h"
+
+int main()
+{
+    InitWindow(800, 450, "Dino");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Dino Game", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
